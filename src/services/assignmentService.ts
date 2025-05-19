c97ac344e8563b0b5431ffb790c2ce473efed88f
ANSHUL JAIN <anshul.jain.devx@gmail.com>
2025-05-19 17:46:11 +0530

    Update assignmentService.ts

--- src/services/assignmentService.ts
+++ src/services/assignmentService.ts
@@ -504,7 +504,7 @@
 import { Assignment, AssignmentFormValues, AssignmentSubmission } from "@/types/assignment";
 
 // API endpoints
-const API_BASE_URL = "https://assignment-generator-403893624463.us-central1.run.app/api";
+const API_BASE_URL = "https://assignment-generator-service-1039810289993.europe-west1.run.app/api"; //https://assignment-generator-403893624463.us-central1.run.app/api";
 const AUTH_TOKEN_URL = "https://my-sign-403893624463.us-central1.run.app/auth/token";
 
 /**
